7b5ae9df714b4c16321adb27934d24b92a9f40af
Pratik Agarwal <pratikagarwal0603@gmail.com>
2025-08-21 19:39:08 +0530

    Add files via upload

---
+++ Day1_pr1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    int a,b,sum;
+    printf("Enter the number a: ");
+    scanf("%d", &a);
+    printf("Enter the number b: ");
+    scanf("%d", &b);
+    printf("The sum of the numbers is %d and %d",a,b, sum = a + b);
+    return 0;
+}
