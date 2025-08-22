c75ed401b9af8933cd2f1b3841bb1e835fc81c9e
Pratik Agarwal <pratikagarwal0603@gmail.com>
2025-08-22 10:22:46 +0530

    Day1_pr1.c

--- Day1_pr1.c
+++ Day1_pr1.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
 
 int main(){
-    int a,b,sum;
+    int num1,num2,sum;
     printf("Enter the number a: ");
-    scanf("%d", &a);
+    scanf("%d", &num1);
     printf("Enter the number b: ");
-    scanf("%d", &b);
-    printf("The sum of the numbers is %d and %d",a,b, sum = a + b);
+    scanf("%d", &num2);
+    printf("The sum of the numbers is %d and %d",num1,num2, sum = num1 + num2);
     return 0;
 }
+
