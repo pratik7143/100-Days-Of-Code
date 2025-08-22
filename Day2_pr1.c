e813f803f6b247ab41752f31c20168fe37f1f56f
Pratik Agarwal <pratikagarwal0603@gmail.com>
2025-08-22 10:41:36 +0530

    Add files via upload

---
+++ Day2_pr1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int length, breadth, perimeter;
+    printf("Enter the length of the rectangle: ");
+    scanf("%d", &length);
+    printf("Enter the breadth of the rectangle: ");
+    scanf("%d", &breadth);
+    printf("The area of the rectangle is %d\n", length*breadth);
+    perimeter = 2*(length+breadth);
+    printf("The perimeter of the rectangle is %d\n", perimeter);
+    return 0;
+}
