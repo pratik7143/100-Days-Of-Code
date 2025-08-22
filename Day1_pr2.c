5cf96cf671ae29b44b9e27da59b1bd05ea5bd5b4
Pratik Agarwal <pratikagarwal0603@gmail.com>
2025-08-22 10:25:03 +0530

    Day1_pr2.c

--- Day1_pr2.c
+++ Day1_pr2.c
@@ -1,14 +1,15 @@
 #include<stdio.h>
 
 int main(){
-    int a,b;
-    printf("Enter the number a: ");
-    scanf("%d", &a);
-    printf("Enter the number b: ");
-    scanf("%d", &b);
-    printf("The sum of the numbers %d and %d is %d\n",a,b,a + b);
-    printf("The product of %d and %d is %d\n", a, b, a * b);
-    printf("The difference of %d and %d is %d\n", a, b, a - b);
-    printf("The quotient of %d and %d is %d\n", a, b, a / b);
+    int num1,num2;
+    printf("Enter the number num1: ");
+    scanf("%d", &num1);
+    printf("Enter the number num2: ");
+    scanf("%d", &num2);
+    printf("The sum of the numbers %d and %d is %d\n",num1,num2,num1+ =num2);
+    printf("The product of %d and %d is %d\n", num1, num2, num1 * num2);
+    printf("The difference of %d and %d is %d\n", num1, num2, num1 - num2);
+    printf("The quotient of %d and %d is %d\n", num1, num2, num1 / num2);
     return 0;
 }
+
