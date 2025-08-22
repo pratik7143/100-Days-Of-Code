e9328d308a1107f01d618386e7c85b4fa13850ad
Pratik Agarwal <pratikagarwal0603@gmail.com>
2025-08-22 10:41:54 +0530

    Add files via upload

---
+++ Day2_pr2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int radius;
+    printf("Enter the radius of the Circle: ");
+    scanf("%d", &radius);
+    printf("The area of the circle with radius %d is %f\n", radius, 3.14 * radius * radius);
+    printf("The circumference of the circle with radius %d is %f\n", radius, 2 * 3.14 * radius);
+    return 0;
+}
