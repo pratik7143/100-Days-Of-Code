b346f8e2ddfa4317d6447ce466017ec94863acfd
Pratik Agarwal <pratikagarwal0603@gmail.com>
2025-08-23 16:43:23 +0530

    Add files via upload

---
+++ Day3_pr1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    float celsius;
+    printf("Enter the temperature: ");
+    scanf("%f", &celsius);
+    float fahrenheit;
+    fahrenheit = (9.0/5.0)*celsius + 32;
+    printf("The value in Fahrenheit is %f", fahrenheit);
+    return 0;
+}
