e8cff4b50a78769c2f3933b1ca8828d29481f946
Pratik Agarwal <pratikagarwal0603@gmail.com>
2025-08-23 19:03:00 +0530

    Day3_pr2.c

--- Day3_pr2.c
+++ Day3_pr2.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 int main() {
-    int num1, num2, temp;
+    int num1, num2, replace;
     printf("Enter two numbers: ");
     scanf("%d %d", &num1, &num2);
-    temp = num1;
+    replace = num1;
     num1 = num2;
-    num2 = temp;
+    num2 = replace;
     printf("The numbers are %d & %d ", num1, num2);
     return 0;
 }
+
