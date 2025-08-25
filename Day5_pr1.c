f3a07041a9874f6c8197f6c6bfb03c715e427c06
Pratik Agarwal <pratikagarwal0603@gmail.com>
2025-08-25 20:41:44 +0530

    Day 5 problem 1

---
+++ Day5_pr1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    float principal, rate, time, amount, si, ci;
+    printf("Enter the pricipal amount: ");
+    scanf("%f", &principal);
+    printf("Enter the Rate of Interest: ");
+    scanf("%f", &rate);
+    printf("The time is: ");
+    scanf(" %f", &time);
+    si = (principal * rate * time) / 100; //si denotes the Simple Interest
+    amount = principal*pow((1+rate/100), time);
+    ci = amount -principal; //ci denotes the Compound Interest
+    printf("The simple interest is %f\n", si);
+    printf("The compound interest is %f\n", ci);
+}
