cd47160759900c22efbd876fe709c6ddacb22ae5
Pratik Agarwal <pratikagarwal0603@gmail.com>
2025-08-28 16:43:37 +0530

    Day 8 problem 1

---
+++ Day8_pr1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    char ch;
+    printf("Enter a character: ");
+    scanf("%c", &ch);
+    if(ch >= 97 && ch <= 122){
+        printf("The character %c is a lowercase alphabet\n", ch);
+    }
+    else if(ch >= 65 && ch <= 90){
+        printf("The character %c is an Uppercase alphabet\n", ch);
+    }
+    else if(ch >= 48 && ch <= 57){
+        printf("The character %c is a digit\n", ch);
+    }
+    else{
+        printf("The character %c is a special character", ch);
+    }
+    return 0;
+}
