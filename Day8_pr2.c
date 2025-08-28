fcf4a0de2910b995e3dadca2990f8a45f83e024a
Pratik Agarwal <pratikagarwal0603@gmail.com>
2025-08-28 16:44:21 +0530

    Day 8 problem 2

---
+++ Day8_pr2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int num1, num2, num3;
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &num1, &num2, &num3);
+    if (num1 > num2 || num1 > num3)
+    {
+        printf("%d is the largest", num1);
+    }
+    else if (num2 > num3 || num2 > num1)
+    {
+        printf("%d is the largest", num2);
+    }
+    else
+    {
+        printf("%d is the largest", num3);
+    }
+    return 0;
+}
