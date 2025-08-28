249055c30dabc5b848483c5041896bab702c8db3
Pratik Agarwal <pratikagarwal0603@gmail.com>
2025-08-28 16:45:11 +0530

    Day8_pr1.c

--- Day8_pr1.c
+++ Day8_pr1.c
@@ -3,17 +3,22 @@ int main(){
     char ch;
     printf("Enter a character: ");
     scanf("%c", &ch);
-    if(ch >= 97 && ch <= 122){
+    if(ch >= 97 && ch <= 122)
+    {
         printf("The character %c is a lowercase alphabet\n", ch);
     }
-    else if(ch >= 65 && ch <= 90){
+    else if(ch >= 65 && ch <= 90)
+    {
         printf("The character %c is an Uppercase alphabet\n", ch);
     }
-    else if(ch >= 48 && ch <= 57){
+    else if(ch >= 48 && ch <= 57)
+    {
         printf("The character %c is a digit\n", ch);
     }
-    else{
+    else
+    {
         printf("The character %c is a special character", ch);
     }
     return 0;
 }
+
