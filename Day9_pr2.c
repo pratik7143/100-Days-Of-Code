8a06ed876233bc60082114f5b497028e71a92f9d
Pratik Agarwal <pratikagarwal0603@gmail.com>
2025-08-29 20:58:13 +0530

    Day 9 problem 2

---
+++ Day9_pr2.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main()
+{
+    
+    float percentage;
+    printf("Enter the percentage: ");
+    scanf("%f", &percentage);
+    if (percentage >= 90)
+    {
+        printf("Grade: A\n");
+    }
+    else if (percentage >= 80)
+    {
+        printf("Grade: B\n");
+    }
+    else if (percentage >= 70)
+    {
+        printf("Grade: C\n");
+    }
+    else if (percentage >= 60)
+    {
+        printf("Grade: D\n");
+    }
+    else if (percentage >= 50)
+    {
+        printf("Grade: E\n");
+    }
+    else if (percentage >= 0)
+    {
+        printf("Grade: F\n");
+    }
+    else
+    {
+        printf("Invalid percentage!\n");
+    }
+    return 0;
+}
