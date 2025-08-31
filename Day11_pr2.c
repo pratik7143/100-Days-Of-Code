2c25215f60fc6eae6d236421b82473c87187a087
Pratik Agarwal <pratikagarwal0603@gmail.com>
2025-08-31 19:28:14 +0530

    Day 11 problem 2

---
+++ Day11_pr2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+    float cp, sp, profit, loss, percent;
+
+    printf("Enter Cost Price: ");
+    scanf("%f", &cp);
+
+    printf("Enter Selling Price: ");
+    scanf("%f", &sp);
+
+    if (sp > cp)
+    {
+        profit = sp - cp;
+        percent = (profit / cp) * 100;
+        printf("Profit Percentage = %f\n", percent);
+    }
+    else if (cp > sp)
+    {
+        loss = cp - sp;
+        percent = (loss / cp) * 100;
+        printf("Loss = %f\n", loss);
+        printf("Loss Percentage = %f\n", percent);
+    }
+    else
+    {
+        printf("No Profit, No Loss.\n");
+    }
+
+    return 0;
+}
