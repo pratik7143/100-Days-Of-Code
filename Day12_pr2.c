d47c5573586c5010fb1bd15c399411d976ec6ee7
Pratik Agarwal <pratikagarwal0603@gmail.com>
2025-09-01 23:21:10 +0530

    Day 12 problem 2

---
+++ Day12_pr2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    int units, bill = 0;
+
+    printf("Enter units consumed: ");
+    scanf("%d", &units);
+
+    if (units <= 100) {
+        bill = units * 5;   // ₹5 per unit
+    }
+    else if (units <= 101 || units <= 200){
+        bill = (units * 5) + 200;
+    }
+    else {
+        bill = (units*5) + 950;
+    }
+    printf("Bill: ₹%d\n", bill);
+}
+ 
