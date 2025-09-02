820c519788bd8b5a9a296f7cd4722b018a64db63
Pratik Agarwal <pratikagarwal0603@gmail.com>
2025-09-02 21:20:37 +0530

    Day 13 problem 1

---
+++ Day13_pr1.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main()
+{
+    float num1, num2;
+    char operator;
+    printf("Enter two numbers = ");
+    scanf("%f %f", &num1, &num2);
+    printf("Enter the operation you want to perform (+, -, *, /, %):  ");
+    scanf(" %c", &operator);
+
+    switch (operator)
+    {
+    case '+':
+        printf("%.1f + %.1f = %.1f", num1, num2, num1 + num2);
+        break;
+    case '-':
+        printf("%.1f - %.1f = %.1f", num1, num2, num1 - num2);
+        break;
+    case '*':
+        printf("%.1f * %.1f = %.1f", num1, num2, num1 * num2);
+        break;
+    case '/':
+        if (num2 != 0)
+            printf("%.1f / %.1f = %.1f", num1, num2, num1 / num2);
+        else
+            printf("Error: Division by zero is not allowed.");
+        break;
+    case '%':
+        if ((int)num2 != 0)
+            printf("%d %% %d = %d", (int)num1, (int)num2, (int)num1 % (int)num2);
+        else
+            printf("Error: Modulus by zero is not allowed.");
+        break;
+    default:
+        printf("Error: Invalid operator.");
+    }
+    return 0;
+}
