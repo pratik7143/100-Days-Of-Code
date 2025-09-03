9411f5ffc709f7300f25748af86792314a6b164f
Pratik Agarwal <pratikagarwal0603@gmail.com>
2025-09-03 10:21:25 +0530

    Day 14 problem 2

---
+++ Day14_pr2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int n, i, pro = 1;
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++)
+    {
+        if (i % 2 == 0)
+        {
+            pro = pro * i;
+        }
+    }
+    printf("Product of even numbers ranging from 1 to %d is %d\n", n, pro);
+    return 0;
+}
