c17e7496ad6b2534138093d0a3e308f6e427df33
Pratik Agarwal <pratikagarwal0603@gmail.com>
2025-09-03 10:23:30 +0530

    Day14_pr1.c

--- Day14_pr1.c
+++ Day14_pr1.c
@@ -3,11 +3,12 @@ int main(){
     int n, i, sum = 0;
     printf("Enter the value of n: ");
     scanf("%d", &n);
-
-    for (i = 1; i <= 2*n; i += 2) {
+    for (i=1; i<=2*n; i=i+2) 
+    {
         sum= sum + i;
     }
     printf("Sum of first %d odd numbers is %d\n", n, sum);
     return 0;
 }
+
 
