d78896727fa9a4ae8369522df338e5a69e43167d
Pratik Agarwal <pratikagarwal0603@gmail.com>
2025-09-03 10:20:52 +0530

    Day 14 problem 1

---
+++ Day14_pr1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int n, i, sum = 0;
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= 2*n; i += 2) {
+        sum= sum + i;
+    }
+    printf("Sum of first %d odd numbers is %d\n", n, sum);
+    return 0;
+}
+
