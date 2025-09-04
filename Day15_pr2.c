935fba3f57ae921591e0896dccd9e9e74857d974
Pratik Agarwal <pratikagarwal0603@gmail.com>
2025-09-04 20:52:04 +0530

    Day15_pr2.c

--- Day15_pr2.c
+++ Day15_pr2.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-
-int main() {
+int main(){
     int n, rn = 0,i; // rn denotes reverse number 
     printf("Enter a number: ");
     scanf("%d", &n);
-    while (n!= 0) {
+    while (n!= 0){
         i = n % 10;          
         rn = rn * 10 + i;    
         n = n / 10;         
@@ -12,3 +11,4 @@ int main() {
     printf("Reversed number = %d\n", rn);
     return 0;
 }
+
