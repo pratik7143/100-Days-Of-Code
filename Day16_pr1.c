0c306ef3c43026a2b01e688b8684d743e8a447e3
Pratik Agarwal <pratikagarwal0603@gmail.com>
2025-09-05 22:23:14 +0530

    Day16_pr1.c

--- Day16_pr1.c
+++ Day16_pr1.c
@@ -1,25 +1,27 @@
 #include <stdio.h>
-
 int main() {
     int num, i;
     int binary[64];  
     printf("Enter a number: ");
     scanf("%d", &num);
-    if (num == 0) {
+    if (num == 0) 
+    {
         printf("Binary: 0\n");
         return 0;
     }
     i = 0;
-    while (num > 0) {
+    while (num > 0) 
+    {
         binary[i] = num % 2;
         num = num / 2;
         i++;
     }
     printf("Binary: ");
-    for (int j = i - 1; j >= 0; j--) {
+    for (int j = i - 1; j >= 0; j--) 
+    {
         printf("%d", binary[j] );
     }
     printf("\n");
-
     return 0;
 }
+
