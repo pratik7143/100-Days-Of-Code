59e053f7eedbb85ea7feb70f75f5c0714f80390a
Pratik Agarwal <pratikagarwal0603@gmail.com>
2025-09-05 22:23:55 +0530

    Day16_pr2.c

--- Day16_pr2.c
+++ Day16_pr2.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
-
-int main() {
+int main(){
     int num, original, reversed = 0, digit;
     printf("Enter a number: ");
     scanf("%d", &num);
     original = num;
-    while (num > 0) {
+    while (num > 0) 
+    {
         digit = num % 10;
         reversed = reversed * 10 + digit;
         num = num / 10;
     }
-    if (original == reversed) {
+    if (original == reversed) 
+    {
         printf("%d is a palindrome.\n", original);
-    } else {
+    } else 
+    {
         printf("%d is not a palindrome.\n", original);
     }
     return 0;
 }
+
