12054100461c9279bb6205d22c5a6ec28a4a688d
Pratik Agarwal <pratikagarwal0603@gmail.com>
2025-09-06 21:34:47 +0530

    Day 17 problem 2

---
+++ Day17_pr2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    int a = 0;
+    for (int i = 2; i <= n - 1; i++)
+    {
+        if (n % i == 0)
+        {
+            a = 1;
+            break;
+        }
+    }
+    if(n==1)
+    printf("1 is neither prime nor composite.");
+    else if (a == 0)
+        printf("The given number is prime number");
+    else
+        printf("The given number is not a prime number");
+    return 0;
+}
