d5b91a90c72c26546214ef278be938792e698c18
Pratik Agarwal <pratikagarwal0603@gmail.com>
2025-09-07 21:45:36 +0530

    Day 18 problem 2

---
+++ Day18_pr2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    int a, b;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    while (b != 0) {
+        a = a % b;
+        if (a == 0) break;
+        int c = a;
+        a = b;
+        b = c;
+    }
+    printf("HCF is %d\n", b == 0 ? a : b);
+    return 0;
+}
