3a801de5e071ce3d62e8eca2400a6ae86ad11521
Pratik Agarwal <pratikagarwal0603@gmail.com>
2025-09-08 22:13:42 +0530

    Day 19 problem 2

---
+++ Day19_pr2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int n, sum = 0, dg;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    while (n != 0)
+    {
+        dg = n % 10;    
+        n = n / 10;        
+        sum = sum + dg; 
+    }
+    printf("Sum of digits is %d\n", sum);
+    return 0;
+}
