d1268033536e9e915ac54197ba706a34cf54db06
Pratik Agarwal <pratikagarwal0603@gmail.com>
2025-09-09 23:11:23 +0530

    Day 20 problem 1

---
+++ Day20_pr1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main() {
+    int num, digit, product = 1;
+    int odd = 0;  // to check if there is at least one odd digit
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    if (num == 0) {
+        printf("Product of odd digits = 0\n");
+        return 0;
+    }
+    while (num > 0) {
+        digit = num % 10;   // extract last digit
+        if (digit % 2 != 0) {  // check if odd
+            product *= digit;
+            odd = 1;
+        }
+        num = num / 10;   // remove last digit
+    }
+    if (odd)
+        printf("Product of odd digits is %d\n", product);
+    else
+        printf("No odd digits found.\n");
+
+    return 0;
+}
