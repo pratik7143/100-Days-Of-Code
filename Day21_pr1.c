85843f9d2d5bbb4543334fae9b03207dd1b22a3f
Pratik Agarwal <pratikagarwal0603@gmail.com>
2025-09-11 22:23:19 +0530

    Day 21 problem 1

---
+++ Day21_pr1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+    int n, f, l, p = 1, t, s;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    l = n % 10;      // last digit
+    t = n;
+    while (t >= 10) {   // find first digit and place value
+        t = t / 10;
+        p = p * 10;
+    }
+    f = t;   // first digit
+    // swap
+    s = l * p + (n % p) / 10 * 10 + f;
+    
+    printf("After swapping: %d\n", s);
+    return 0;
+}
