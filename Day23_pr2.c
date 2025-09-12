2e7f4b641ee2e659f071d5dfb5d0922b534bbf0a
Pratik Agarwal <pratikagarwal0603@gmail.com>
2025-09-12 22:02:03 +0530

    Day 23 problem 2

---
+++ Day23_pr2.c
@@ -0,0 +1,33 @@
+/*Q46: Write a program to print the following pattern:
+*****
+*****
+*****
+*****
+*****
+
+/*
+Sample Test Cases:
+Input 1:
+
+Output 1:
+*****
+*****
+*****
+*****
+*****
+
+*/
+#include<stdio.h>
+int main() {
+    //int n;                                  
+    //printf("Enter the number of rows: ");             For n number of rows
+    //scanf("%d",&n);
+    for(int i = 1;i<=5;i++){
+        for (int j = 1;j<=5;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
