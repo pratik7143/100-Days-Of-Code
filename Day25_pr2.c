1d011de415780eb0aae359a66e75748099ea6775
Pratik Agarwal <pratikagarwal0603@gmail.com>
2025-09-14 22:39:19 +0530

    Day 25 problem 2

---
+++ Day25_pr2.c
@@ -0,0 +1,33 @@
+/*Q50: Write a program to print the following pattern:
+*****
+  ****
+    ***
+      **
+        *
+Sample Test Cases:
+Input 1:
+Output 1:
+*****
+  ****
+    ***
+      **
+        *
+
+Input 2:
+Output 2:
+Note: Spaces indicate indentation.
+*/
+#include<stdio.h>
+int main() {
+    //int n;                                  
+    //printf("Enter the number of rows: ");             For n number of rows
+    //scanf("%d",&n);
+    for(int i =5;i>=1;i--){
+        for (int j = 1;j<=i;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
