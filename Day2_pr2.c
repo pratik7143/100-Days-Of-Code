922feaadf71e2db698bffe1d452714b50a275045
Pratik Agarwal <pratikagarwal0603@gmail.com>
2025-09-15 22:55:24 +0530

    Day2_pr2.c

--- Day2_pr2.c
+++ Day2_pr2.c
@@ -1,5 +1,19 @@
-#include <stdio.h>
+/*Q4: Write a program to calculate the area and circumference of a circle given its radius.
 
+/*
+Sample Test Cases:
+Input 1:
+7
+Output 1:
+Area=153.94, Circumference=43.96
+
+Input 2:
+3
+Output 2:
+Area=28.27, Circumference=18.85
+
+*/
+#include <stdio.h>=
 int main()
 {
     int radius;
@@ -9,4 +23,5 @@ int main()
     printf("The circumference of the circle with radius %d is %f\n", radius, 2 * 3.14 * radius);
     return 0;
 }
+
 
