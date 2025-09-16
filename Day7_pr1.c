321171a8d14bd5a3728abf387d4942ecfae3afff
Pratik Agarwal <pratikagarwal0603@gmail.com>
2025-09-16 21:44:55 +0530

    Day7_pr1.c

--- Day7_pr1.c
+++ Day7_pr1.c
@@ -1,3 +1,23 @@
+Q13: Write a program to input a year and check whether it is a leap year or not using conditional statements.
+
+/*
+Sample Test Cases:
+Input 1:
+2020
+Output 1:
+Leap year
+
+Input 2:
+1900
+Output 2:
+Not a leap year
+
+Input 3:
+2000
+Output 3:
+Leap year
+
+*/
 #include<stdio.h>
 int main(){
     int year;
@@ -14,5 +34,6 @@ int main(){
     }
     return 0;
 }
+
 
 
