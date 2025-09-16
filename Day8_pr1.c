05dfb36a45323a29662957d73db5227ff8cdbc0e
Pratik Agarwal <pratikagarwal0603@gmail.com>
2025-09-16 21:46:18 +0530

    Day8_pr1.c

--- Day8_pr1.c
+++ Day8_pr1.c
@@ -1,3 +1,28 @@
+Q15: Write a program to input a character and check whether it is an uppercase alphabet, lowercase alphabet, digit, or special character.
+
+/*
+Sample Test Cases:
+Input 1:
+A
+Output 1:
+Uppercase alphabet
+
+Input 2:
+a
+Output 2:
+Lowercase alphabet
+
+Input 3:
+3
+Output 3:
+Digit
+
+Input 4:
+#
+Output 4:
+Special character
+
+*/
 #include<stdio.h>
 int main(){
     char ch;
@@ -21,4 +46,5 @@ int main(){
     }
     return 0;
 }
+
 
