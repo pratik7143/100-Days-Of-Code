7ec449ca3b3baae4bc7dc0bc26e62a92cf2b3015
Pratik Agarwal <pratikagarwal0603@gmail.com>
2025-09-17 21:50:11 +0530

    Day 27 problem 1

---
+++ Day27_pr1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    //int n;
+    //printf("Enter the number of lines: ");
+    //scanf("%d",&n);
+    for(int i = 1;i<5;i++){
+        for(int j=1;j<=2*i-1;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    for(int i = 4; i >= 1; i--) {
+        for (int j = 1; j <=2*i-1; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
