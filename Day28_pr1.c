0b5bc029fc3be3d362329804e22aa2ae51a48357
Pratik Agarwal <pratikagarwal0603@gmail.com>
2025-09-17 21:42:30 +0530

    Day 28 problem 1

---
+++ Day28_pr1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+    printf("Prime numbers from 1 to %d are:\n", n);
+    for (int i = 2; i <= n; i++) {        
+        int j;
+        for (j = 2; j < i; j++){       
+            if (i % j == 0){          
+                break;
+            }
+        }
+        if (j == i) {                   
+            printf("%d ", i);
+        }
+    }
+    return 0;
+}
