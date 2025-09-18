39c05f90efe456a7194ca407823aef8057e9254d
Pratik Agarwal <pratikagarwal0603@gmail.com>
2025-09-18 23:44:32 +0530

    Day11_pr2.c

--- Day11_pr2.c
+++ Day11_pr2.c
@@ -1,3 +1,23 @@
+Q22: Write a program to find profit or loss percentage given cost price and selling price.
+
+/*
+Sample Test Cases:
+Input 1:
+1000 1200
+Output 1:
+Profit 20%
+
+Input 2:
+1000 800
+Output 2:
+Loss 20%
+
+Input 3:
+1000 1000
+Output 3:
+No Profit No Loss
+
+*/
 #include <stdio.h>
 int main()
 {
@@ -25,4 +45,5 @@ int main()
     }
     return 0;
 }
+
 
