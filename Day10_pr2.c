c7e9bfd0ab06aae9528c81de9a5682057d410014
Pratik Agarwal <pratikagarwal0603@gmail.com>
2025-09-18 23:43:47 +0530

    Day10_pr2.c

--- Day10_pr2.c
+++ Day10_pr2.c
@@ -1,3 +1,18 @@
+Q20: Write a program to display the day of the week based on a number (1–7) using switch-case.
+
+/*
+Sample Test Cases:
+Input 1:
+1
+Output 1:
+Monday
+
+Input 2:
+5
+Output 2:
+Friday
+
+*/
 #include <stdio.h>
 int main() {
     int choice ;
@@ -30,4 +45,5 @@ int main() {
     }
     return 0;
 }
+
 
