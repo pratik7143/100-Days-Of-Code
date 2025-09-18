b00a113701fe7157715fe0456b113e4eb4da3a72
Pratik Agarwal <pratikagarwal0603@gmail.com>
2025-09-18 23:43:11 +0530

    Day10_pr1.c

--- Day10_pr1.c
+++ Day10_pr1.c
@@ -1,3 +1,23 @@
+Q19: Write a program to classify a triangle as Equilateral, Isosceles, or Scalene based on its side lengths.
+
+/*
+Sample Test Cases:
+Input 1:
+3 3 3
+Output 1:
+Equilateral
+
+Input 2:
+3 3 4
+Output 2:
+Isosceles
+
+Input 3:
+2 3 4
+Output 3:
+Scalene
+
+*/
 #include <stdio.h>
 int main(){
     float a, b, c; // a,b,c are the three sides of the triangle
@@ -25,4 +45,5 @@ int main(){
         }
         return 0;
     }
+
 
