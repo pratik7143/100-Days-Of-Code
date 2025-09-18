7bc9b6e4665538d5c7eff01625b577a92621f5cb
Pratik Agarwal <pratikagarwal0603@gmail.com>
2025-09-18 23:44:09 +0530

    Day11_pr1.c

--- Day11_pr1.c
+++ Day11_pr1.c
@@ -1,3 +1,18 @@
+Q21: Write a program to display the month name and number of days using switch-case for a given month number.
+
+/*
+Sample Test Cases:
+Input 1:
+2
+Output 1:
+February, 28 days
+
+Input 2:
+12
+Output 2:
+December, 31 days
+
+*/
 #include <stdio.h>
 int main() {
     int month;
@@ -45,4 +60,5 @@ int main() {
     }
     return 0;
 }
+
 
