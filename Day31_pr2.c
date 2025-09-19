2fcd9cb0b4476ced41c24c789582c61be7402692
Pratik Agarwal <pratikagarwal0603@gmail.com>
2025-09-20 02:13:01 +0530

    Day 31 problem 1

---
+++ Day31_pr2.c
@@ -0,0 +1,31 @@
+/*Q62: Reverse an array without taking extra space.
+
+/*
+Sample Test Cases:
+Input 1:
+4
+1 2 3 4
+Output 1:
+4 3 2 1
+
+*/
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("Enter the number of elements you want in the array:\n");
+    scanf("%d", &n);
+    int array[n];
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &array[i]);
+    }
+    printf("The array in the reverse order will be:\n");
+    for (int i = n - 1; i >= 0; i--)
+    {
+        printf("%d ", array[i]);
+    }
+
+    return 0;
+}
