b47fc9f1c2639afe39e5e96b8658808c51faad3e
Pratik Agarwal <pratikagarwal0603@gmail.com>
2025-09-21 23:05:35 +0530

    Day32_pr2.c

--- Day32_pr2.c
+++ Day32_pr2.c
@@ -1,16 +1,16 @@
 /*Q64: Find the digit that occurs the most times in an integer number.
-
 /*
 Sample Test Cases:
 Input 1:
 112233
 Output 1:
 1
-
 Input 2:
 887799
 Output 2:
 7
+
+
 
 */
 #include <stdio.h>
@@ -39,3 +39,4 @@ int main()
     printf("The digit that occurs the most is: %d\n", maxDigit);
     return 0;
 }
+
