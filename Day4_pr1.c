d85ce227bb3aebe39136d7bbda56f83c0d11a1c6
Pratik Agarwal <pratikagarwal0603@gmail.com>
2025-09-22 21:17:47 +0530

    Day4_pr1.c

--- Day4_pr1.c
+++ Day4_pr1.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-
-int main() {
+int main(){
     int num1,num2;
     printf("Enter two numbers: ");
     scanf("%d %d", &num1, &num2);
@@ -10,3 +9,4 @@ int main() {
     printf("The numbers are %d & %d\n", num1, num2);
     return 0;
 }
+
