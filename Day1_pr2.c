a101ec95497c86ded986f5e4599d8a02850e7ae4
Pratik Agarwal <pratikagarwal0603@gmail.com>
2025-09-22 21:13:15 +0530

    Day1_pr2.c

--- Day1_pr2.c
+++ Day1_pr2.c
@@ -4,12 +4,10 @@ Input 1:
 10 2
 Output 1:
 Sum=12, Diff=8, Product=20, Quotient=5
-
 Input 2:
 7 3
 Output 2:
 Sum=10, Diff=4, Product=21, Quotient=2
-
 */
 #include<stdio.h>
 int main(){
@@ -24,5 +22,6 @@ int main(){
     printf("The quotient of %d and %d is %d\n", num1, num2, num1 / num2);
     return 0;
 }
+
 
 
