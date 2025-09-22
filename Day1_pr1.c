f1d977906e373004a3fd6ebfbf0173e0017980c2
Pratik Agarwal <pratikagarwal0603@gmail.com>
2025-09-22 21:12:18 +0530

    Day1_pr1.c

--- Day1_pr1.c
+++ Day1_pr1.c
@@ -1,17 +1,13 @@
 /*Q1: Write a program to input two numbers and display their sum.
-
-/*
 Sample Test Cases:
 Input 1:
 3 4
 Output 1:
 Sum = 7
-
 Input 2:
 -1 20
 Output 2:
 Sum = 19
-
 */
 #include<stdio.h>
 int main(){
@@ -23,5 +19,6 @@ int main(){
     printf("The sum of the numbers is %d and %d",num1,num2, sum = num1 + num2);
     return 0;
 }
+
 
 
