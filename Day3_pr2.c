98cba17bcdbc432ccf0b53a685fbca31d08ff908
Pratik Agarwal <pratikagarwal0603@gmail.com>
2025-09-22 21:17:09 +0530

    Day3_pr2.c

--- Day3_pr2.c
+++ Day3_pr2.c
@@ -1,20 +1,16 @@
 /*Q6: Write a program to swap two numbers using a third variable.
-
-/*
 Sample Test Cases:
 Input 1:
 3 5
 Output 1:
 After swap: 5 3
-
 Input 2:
 -1 1
 Output 2:
 After swap: 1 -1
-
 */
-#include <stdio.h>
-int main() {
+#include<stdio.h>
+int main(){
     int num1, num2, replace;
     printf("Enter two numbers: ");
     scanf("%d %d", &num1, &num2);
@@ -24,5 +20,6 @@ int main() {
     printf("The numbers are %d & %d ", num1, num2);
     return 0;
 }
+
 
 
