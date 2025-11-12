da6473c929d0e023daf50551d3880793332a83de
Pratik Agarwal <pratikagarwal0603@gmail.com>
2025-11-12 22:35:55 +0530

    Day 41 problem

---
+++ Day41_pr2.c
@@ -0,0 +1,25 @@
+// Q82: Print each character of a string on a new line.
+
+/*
+Sample Test Cases:
+Input 1:
+Hi
+Output 1:
+H
+i
+
+*/
+
+#include <stdio.h>
+int main()
+{
+    char str[100];
+    int i = 0;
+    scanf("%[^\n]s", str);
+    while (str[i] != '\0')
+    {
+        printf("%c\n", str[i]);
+        i++;
+    }
+    return 0;
+}
