ccb711eb3a68b184d656c0d0df5c1b3e5ffe49d3
Pratik Agarwal <pratikagarwal0603@gmail.com>
2025-11-14 22:49:04 +0530

    Day 49 problem 2

---
+++ Day49_pr2.c
@@ -0,0 +1,21 @@
+// Q98: Print initials of a name with the surname displayed in full.
+
+/*
+Sample Test Cases:
+Input 1:
+John David Doe
+Output 1:
+J.D. Doe
+
+*/
+
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char first[30], middle[30], last[30];
+    printf("enter the name: ");
+    scanf("%s %s %s", first, middle, last);
+    printf("%c.%c. %s\n", first[0], middle[0], last);
+    return 0;
+}
