15b966d7443efe77d2480e2066360d5c2ab28894
Pratik Agarwal <pratikagarwal0603@gmail.com>
2025-11-14 22:50:06 +0530

    Day 50 problem 1

---
+++ Day50_pr1.c
@@ -0,0 +1,27 @@
+// Q99: Change the date format from dd/04/yyyy to dd-Apr-yyyy.
+
+/*
+Sample Test Cases:
+Input 1:
+15/04/2025
+Output 1:
+15-Apr-2025
+
+*/
+
+#include <stdio.h>
+int main()
+{
+    int day, month, year;
+    char *months[] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun","Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+    scanf("%d/%d/%d", &day, &month, &year);
+    if (month >= 1 && month <= 12)
+    {
+        printf("%02d-%s-%d\n", day, months[month - 1], year);
+    }
+    else
+    {
+        printf("Invalid month entered.\n");
+    }
+    return 0;
+}
